803f81981aa0895b03eec7eb515e0d7e9f22c8a7
PraveenReddy1236 <101916191+PraveenReddy1236@users.noreply.github.com>
2022-03-19 11:08:29 +0530

    aaa

---
+++ Demo/Firstcode.java
@@ -0,0 +1,69 @@
+
+public class Firstcode {
+	public static void main(String args[])
+	{
+		
+		for(int j=1;j<=1;j++)
+		{
+		for(int i=1;i<=1;i++)
+		{
+		System.out.print("1 ");
+		
+		}
+		System.out.println();
+		}
+
+	for(int j=2;j<=2;j++)
+	{
+		for(int i=2;i<=2;i++)
+		{
+			System.out.println("1 2");
+		}
+		
+	}
+	for(int j=3;j<=3;j++)
+	{
+		for(int i=3;i<=3;i++)
+		{
+			System.out.println("1 2 3");
+		}
+	}
+	for(int j=4;j<=4;j++)
+	{
+		for(int i=4;i<=4;i++)
+		{
+			System.out.println("1 2 3 4");
+		}
+	}
+	for(int j=5;j<=5;j++)
+	{
+		for(int i=5;i<=5;i++)
+		{
+			System.out.println("1 2 3 4 5");
+		}
+	}
+	for(int j=6;j<=6;j++)
+	{
+		for(int i=6;i<=6;i++)
+		{
+			System.out.println("1 2 3 4 5 6");
+		}
+	}
+
+
+
+
+				
+		
+		
+		
+		
+		
+		}
+		
+		
+	}
+
+		
+		
+
