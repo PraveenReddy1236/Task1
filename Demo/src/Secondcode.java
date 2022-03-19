fb2ffe824f89c4356f51f96035ed03d182c46e06
nasipireddygaripraveen <nasipireddygaripraveen@172.20.10.2>
2022-03-19 10:35:50 +0530

    First commit
    uploading al the files

---
+++ Demo/src/Secondcode.java
@@ -0,0 +1,36 @@
+
+public class Secondcode {
+
+	public static void main(String[] args) 
+	{
+		for(int j=1;j<=1;j++)
+		{
+			for(int i=1;i<=1;i++)
+			{
+		     System.out.print("A");
+			}
+			}
+		System.out.println();
+		
+		for(int j=2;j<=2;j++)
+		{
+			for(int i=2;i<=2;i++)
+			{
+		     System.out.print("A B");
+			}
+			}
+		System.out.println();
+		for(int j=3;j<=3;j++)
+		{
+			for(int i=3;i<=3;i++)
+			{
+		     System.out.print("A B C");
+			}
+			}
+		System.out.println();
+		
+		
+		}
+
+
+}
---
+++ Demo/src/Thirdcode.java
@@ -0,0 +1,42 @@
+
+public class Thirdcode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int j=1;j<=1;j++)
+		{
+			for(int i=1;i<=1;i++)
+			{
+		     System.out.print("$ $ $ $");
+			}
+			}
+		System.out.println();
+		
+		for(int j=2;j<=2;j++)
+		{
+			for(int i=2;i<=2;i++)
+			{
+		     System.out.print("$     $");
+			}
+			}
+		System.out.println();
+		for(int j=3;j<=3;j++)
+		{
+			for(int i=3;i<=3;i++)
+			{
+		     System.out.print("$     $");
+			}
+			}
+		System.out.println();
+		{
+			
+			{
+		     System.out.print("$ $ $ $");
+			}
+			}
+		System.out.println();
+
+	}
+
+}
